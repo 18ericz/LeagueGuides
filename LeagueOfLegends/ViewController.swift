926554bfd58f70c9b4fee8c5b21a49efa6186352
马蕾 <18ericz@18ericzdemacbook-pro.local>
2019-04-27 18:29:14 -0400

    Initial Commit

---
+++ LeagueOfLegends/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LeagueOfLegends
+//
+//  Created by 18ericz on 4/27/19.
+//  Copyright © 2019 18ericz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
